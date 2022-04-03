2f033450ef12f9f3a4dc1e5e90b9efa2435cc769
mgurga <motiurga@gmail.com>
2022-04-03 03:51:52 -0700

    set server based on build

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,9 @@
 import { writable } from 'svelte/store';
 import { browser } from "$app/env";
 
-export const server_url = writable("http://localhost:5000");
+let isProduction = import.meta.env.MODE === 'production';
+console.log(import.meta.env.MODE);
+export const server_url = isProduction ? writable("https://hiloserver.pythonanywhere.com") : writable("http://localhost:5000");
 
 export let username = writable("");
 export let key = writable("");
